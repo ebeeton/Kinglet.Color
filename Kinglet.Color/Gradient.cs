da8a763ab64076981561dd5f659fc92db903581e
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-02-06 17:17:45 -0500

    Add gradient class stub.

---
+++ Kinglet.Color/Gradient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Kinglet.Color
+{
+	/// <summary>
+	/// A color gradient defined as collection of stops.
+	/// </summary>
+	public class Gradient
+	{
+		/// <summary>
+		/// The stops that make up the gradient.
+		/// </summary>
+		public List<Stop> Stops { get; set; }
+	}
+}
