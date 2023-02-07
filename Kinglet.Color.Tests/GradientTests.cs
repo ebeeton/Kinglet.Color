4b96650ff84cfe80b4aff90581f5d38aa40f4f24
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2023-02-07 17:54:11 -0500

    Add gradient to palette conversion method.

---
+++ Kinglet.Color.Tests/GradientTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Kinglet.Color.Tests
+{
+	[TestClass]
+	public class GradientTests
+	{
+		[TestMethod]
+		public void GetPalette_WithStops_ReturnsExpectedColors()
+		{
+			var black = new Rgba32(0, 0, 0, 255);
+			var grey = new Rgba32(127, 127, 127, 255);
+			var white = new Rgba32(255, 255, 255, 255);
+
+			var gradient = new Gradient
+			{
+				Stops = new List<Stop>
+				{
+					// Black -> white, 100% alpha.
+					new Stop(0, black),
+					new Stop(1, white)
+				}
+			};
+
+			var palette = gradient.GetPalette(3);
+
+			Assert.AreEqual(3, palette.Length);
+			Assert.AreEqual(black.R, palette[0].R);
+			Assert.AreEqual(black.G, palette[0].G);
+			Assert.AreEqual(black.B, palette[0].B);
+			Assert.AreEqual(black.A, palette[0].A);
+			Assert.AreEqual(grey.R, palette[1].R);
+			Assert.AreEqual(grey.G, palette[1].G);
+			Assert.AreEqual(grey.B, palette[1].B);
+			Assert.AreEqual(grey.A, palette[1].A);
+			Assert.AreEqual(white.R, palette[2].R);
+			Assert.AreEqual(white.G, palette[2].G);
+			Assert.AreEqual(white.B, palette[2].B);
+			Assert.AreEqual(white.A, palette[2].A);
+		}
+	}
+}
--- Kinglet.Color/Gradient.cs
+++ Kinglet.Color/Gradient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Kinglet.Color
 {
@@ -8,8 +9,45 @@ namespace Kinglet.Color
 	public class Gradient
 	{
 		/// <summary>
-		/// The stops that make up the gradient.
+		/// The stops that make up the <see cref="Gradient"/>.
 		/// </summary>
 		public List<Stop> Stops { get; set; }
+
+		/// <summary>
+		/// Gets a palette with a given number of colors in the same order as the gradient stops.
+		/// </summary>
+		/// <param name="count">Number of colors in the resulting palette.</param>
+		/// <returns>A palette of colors representi</returns>
+		public Rgba32[] GetPalette(UInt32 count)
+		{
+			var palette = new Rgba32[count];
+
+			// Ensure that the last color is the last stop's color.
+			var step = 1.0 / ((double)count - 1);
+			for (int i = 0; i < count; i++)
+			{
+				palette[i] = GetInterpolatedColor(i * step);
+			}
+			return palette;
+		}
+
+		private Rgba32 GetInterpolatedColor(double position)
+		{
+			Stop first, second;
+			for (int i = 0; i < Stops.Count - 1; i++)
+			{
+				// Which stops is position between?
+				first = Stops[i];
+				second = Stops[i + 1];
+				if (first.Position <= position && position <= second.Position)
+				{
+					// Position is in [0,1] over all stops, scale it to the range between the two colors.
+					var localPosition = (position - first.Position) / (second.Position - first.Position);
+					return first.Color.LinearInterpolate(second.Color, localPosition);
+				}
+			}
+
+			return Stops[Stops.Count - 1].Color;
+		}
 	}
 }
